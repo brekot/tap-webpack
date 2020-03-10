a21f047c76f9afc86b14afa82ca619053c03f331
bas <bas@ya.ru>
2020-03-10 17:01:07 +0300

    ---

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,22 @@
 import $ from "jquery";
 import formstyler from './jquery.formstyler.min.js';
+import { Swiper, Navigation } from 'swiper/js/swiper.esm.js';
+
+Swiper.use([Navigation]);
 
 $(function() {
 
     $('select').styler();
+
+    $('.apartment-detail__show-all').click(function(){
+
+        $(this).toggleClass('apartment-detail__show-all_active');
+        $(this).prev().toggleClass('active');
+    });
+
+    new Swiper ('.apartment-slider__slider', {
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 20,
+    })
 });
