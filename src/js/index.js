ea7c5bf3587ea2cddeb72a050901871019853fd5
bas <bas@ya.ru>
2020-03-19 17:01:30 +0300

    Нет времени объяснять

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,19 @@
 import $ from "jquery";
 import formstyler from './jquery.formstyler.min.js';
+import range from './range.slider.min.js';
 import { Swiper, Navigation } from 'swiper/js/swiper.esm.js';
 
 Swiper.use([Navigation]);
+
+window.jQuery = $;
+window.$ = $;
+
+require('@fancyapps/fancybox');
+
+function numberWithSpaces(x)
+{
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
 
 $(function() {
 
@@ -23,5 +34,32 @@ $(function() {
             nextEl: '.apartment-slider__next',
             prevEl: '.apartment-slider__prev',
         },
-    })
+    });
+
+    if ($("#price-range").length > 0)
+    {
+        $("#price-range").ionRangeSlider({
+            type: "double",
+            grid: false,
+            min: $('#price-range-from').val().replace(/\s+/g,''),
+            max: $('#price-range-to').val().replace(/\s+/g,''),
+            from: $('#price-range-from').val().replace(/\s+/g,''),
+            to: $('#price-range-to').val().replace(/\s+/g,''),
+            onChange: function (data) {
+
+                $('#price-range-from').val(numberWithSpaces(data.from));
+                $('#price-range-to').val(numberWithSpaces(data.to));
+            }
+        });
+    }
+
+    $('[data-fancybox]').fancybox({
+        'touch' : false,
+		'modal' : false,
+    });
+
+    $('.search-filter-btn').click(function(){
+
+        $('.search-filter').toggleClass('search-filter_active');
+    });
 });
